0798b16685e42c42321aa500a13b860ceeb98f9c
NectherLouieDev <necther.louie.dev@gmail.com>
2024-04-23 17:54:34 +0100

    mana mvc
    mana mvc

--- Source/AuraTemplate/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/AuraTemplate/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -12,4 +12,51 @@ void UOverlayWidgetController::BroadcastInitialValues()
 
 	OnHealthChanged.Broadcast(AuraAttributeSet->GetHealth());
 	OnMaxHealthChanged.Broadcast(AuraAttributeSet->GetMaxHealth());
+	OnManaChanged.Broadcast(AuraAttributeSet->GetMana());
+	OnMaxManaChanged.Broadcast(AuraAttributeSet->GetMaxMana());
+}
+
+void UOverlayWidgetController::BindCallbacksToDependencies()
+{
+	Super::BindCallbacksToDependencies();
+
+	const UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	// Health Delegates
+	FOnGameplayAttributeValueChange& HealthValueChangeDelegate = 
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetHealthAttribute());
+	HealthValueChangeDelegate.AddUObject(this, &UOverlayWidgetController::HealthChanged);
+
+	FOnGameplayAttributeValueChange& MaxHealthValueChangeDelegate =
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetMaxHealthAttribute());
+	MaxHealthValueChangeDelegate.AddUObject(this, &UOverlayWidgetController::MaxHealthChanged);
+
+	// Mana Delegates
+	FOnGameplayAttributeValueChange& ManaValueChangeDelegate =
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetManaAttribute());
+	ManaValueChangeDelegate.AddUObject(this, &UOverlayWidgetController::ManaChanged);
+
+	FOnGameplayAttributeValueChange& MaxManaValueChangeDelegate =
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetMaxManaAttribute());
+	MaxManaValueChangeDelegate.AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
+}
+
+void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
+{
+	OnHealthChanged.Broadcast(Data.NewValue);
+}
+
+void UOverlayWidgetController::MaxHealthChanged(const FOnAttributeChangeData& Data) const
+{
+	OnMaxHealthChanged.Broadcast(Data.NewValue);
+}
+
+void UOverlayWidgetController::ManaChanged(const FOnAttributeChangeData& Data) const
+{
+	OnManaChanged.Broadcast(Data.NewValue);
+}
+
+void UOverlayWidgetController::MaxManaChanged(const FOnAttributeChangeData& Data) const
+{
+	OnMaxManaChanged.Broadcast(Data.NewValue);
 }
--- Source/AuraTemplate/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/AuraTemplate/Public/UI/WidgetController/AuraWidgetController.h
@@ -9,6 +9,8 @@
 class UAttributeSet;
 class UAbilitySystemComponent;
 
+struct FOnAttributeChangeData;
+
 USTRUCT(BlueprintType)
 struct FWidgetControllerParams
 {
@@ -44,6 +46,7 @@ public:
 	void SetWidgetControllerParams(const FWidgetControllerParams& WCParams);
 
 	virtual void BroadcastInitialValues();
+	virtual void BindCallbacksToDependencies();
 
 protected:
 	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
